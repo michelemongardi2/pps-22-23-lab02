dc2887ae311378d4735a157c1aedb4fa8192bb0a
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-03-18 19:22:26 +0100

    refactor code with println

--- src/main/scala/u02/task2/Task2a.scala
+++ src/main/scala/u02/task2/Task2a.scala
@@ -6,6 +6,7 @@ object Task2a extends App {
     case x if x >= 0 => "positive"
     case _ => "negative"
 
+  println("******** Task2a a) ********")
   println(positiveVal(10))
   println(positiveVal(0))
   println(positiveVal(-5))
@@ -14,24 +15,27 @@ object Task2a extends App {
     case n if n >= 0 => "positive"
     case _ => "negative"
 
+  println("******** Task2a a) 'def' ********")
   println(positiveDef(4))
   println(positiveDef(0))
   println(positiveDef(-2))
 
   val neg: (String => Boolean) => (String => Boolean) = f => (s => !f(s))
-  def negDef(f: String => Boolean): (String => Boolean) = !f(_)
 
   val empty: String => Boolean = _ == "" // predicate on strings
   val notEmpty = neg(empty) // which type of notEmpty? (String => Boolean)
 
+  println("******** Task2a b) ********")
   println(empty("Hello")) //false
   println(empty("")) //true
   println(notEmpty("foo")) // true
   println(notEmpty ("")) // false
   println(notEmpty ("foo") && !notEmpty("")) // true.. a comprehensive test
 
+  def negDef(f: String => Boolean): (String => Boolean) = !f(_)
   val notEmptyWithNegDef = negDef(empty) // which type of notEmpty? (String => Boolean)
 
+  println("******** Task2a b) 'def' ********")
   println(notEmptyWithNegDef("foo")) // true
   println(notEmptyWithNegDef("")) // false
   println(notEmptyWithNegDef("foo") && !notEmpty("")) // true.. a comprehensive test
@@ -44,6 +48,7 @@ object Task2a extends App {
 
   val notGreaterThanTen: Int => Boolean = (genericNeg(greaterThanTen))
 
+  println("******** Task2a c) ********")
   println(greaterThanTen(11)) // true
   println(greaterThanTen(-5)) // false
   println(notGreaterThanTen(20)) //false
