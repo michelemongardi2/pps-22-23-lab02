95fc5edf71d05f7eed26c1668e993b7d783aafb9
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-03-19 17:12:19 +0100

    Task2b def curriend & notcurried

--- src/main/scala/u02/task2/b/Task2b.scala
+++ src/main/scala/u02/task2/b/Task2b.scala
@@ -11,7 +11,7 @@ object Task2b extends App {
     case x if ((x <= y) && (y == z)) => true
     case _ => false
 
-  println("******** Task2b curriedFunType) ********")
+  println("******** Task2b CurriedFunType) ********")
   println(p1(5, 5)(2)) //true
   println(p1(10, 2)(8)) //false
   println(p1(5, 5)(20)) //false
@@ -22,4 +22,22 @@ object Task2b extends App {
   println(p2(10, 2, 2)) //false
   println(p2(5, 10, 20)) //false
 
+  def p3(x: Int)(y: Int)(z:Int): Boolean = x match
+    case x if ((x <= y) && (y == z)) => true
+    case _ => false
+
+  def p4(x: Int, y: Int, z: Int): Boolean = x match
+    case x if ((x <= y) && (y == z)) => true
+    case _ => false
+
+
+  println("******** Task2b CurriedMethod) ********")
+  println(p3(1)(2)(2)) //true
+  println(p3(9)(8)(8)) //false
+  println(p3(3)(5)(6)) //false
+
+  println("******** Task2b NonCurriedMethod) ********")
+  println(p4(5, 10, 10)) //true
+  println(p4(10, 2, 2)) //false
+  println(p4(5, 10, 20)) //false
 }
